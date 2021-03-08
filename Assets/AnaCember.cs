141f593fcde0e5efb70422bef93fc3a19a8af67c
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2021-03-08 18:15:24 +0300

    Project file added

---
+++ Assets/AnaCember.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnaCember : MonoBehaviour
+{
+    public GameObject kucukCember;
+    GameObject OyunYonetici;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OyunYonetici = GameObject.FindGameObjectWithTag("oyunYoneticisiTag");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            kucukCemberOlustur();
+        }
+    }
+    void kucukCemberOlustur()
+    {
+        Instantiate(kucukCember, transform.position, transform.rotation);
+        OyunYonetici.GetComponent<OyunYoneticisi>().kucukCemberlerdeTextGosterme();
+    }
+}
---
+++ Assets/kucukCemberKod.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kucukCemberKod : MonoBehaviour
+{
+
+    Rigidbody2D fizik;
+    public float hiz;
+    bool hareketkontrol = false;
+    GameObject oyunYoneticisi;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody2D>();
+        oyunYoneticisi = GameObject.FindGameObjectWithTag("oyunYoneticisiTag");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hareketkontrol)
+        {
+            fizik.MovePosition(fizik.position + Vector2.up * hiz * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag=="donenCemberTag")
+        {
+            transform.SetParent(col.transform);
+            hareketkontrol = true;
+        }
+
+        if(col.tag=="kucukCemberTag")
+        {
+            oyunYoneticisi.GetComponent<OyunYoneticisi>().OyunBitti();
+        }
+    }
+}
